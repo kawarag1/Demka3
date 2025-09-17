80f162cc380c41b1dc0f5056f6026abf7edf1990
kawarag1 <dima.kretinin.05@mail.ru>
2025-09-17 22:42:59 +0700

    xdddd

--- Demka3/Pages/CountMaterials.xaml.cs
+++ Demka3/Pages/CountMaterials.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Demka3.Models;
+using Demka3.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,20 +17,45 @@ using System.Windows.Shapes;
 
 namespace Demka3.Pages
 {
+    public class ModelForCountMaterials
+    {
+        public string title;
+        public int counter;
+    }
     /// <summary>
     /// Логика взаимодействия для CountMaterials.xaml
     /// </summary>
     public partial class CountMaterials : Page
     {
+        public static Products product_;
+        public static List<ModelForCountMaterials> materials = new List<ModelForCountMaterials>();
         public CountMaterials(Products product)
         {
             InitializeComponent();
+            product_ = product;
         }
 
 
-        public List<Materials> MaterialsCount(int TypeProductID, int MaterialTypeID, int NumberOfProducts, decimal CountOnStorage, )
+        public void MaterialsCount(int TypeProductID, int MaterialTypeID, int NumberOfProducts, decimal CountOnStorage, decimal BreakPercent, decimal ProductTypeKef, Deemka3Entities db)
         {
-            return null;
+            ModelForCountMaterials material = new ModelForCountMaterials();
+            material.title = db.Materials.Where(x => x.MaterialType_ID == MaterialTypeID).Select(x => x.Title).FirstOrDefault();
+
+            int MaterialsForOneProduct = Convert.ToInt32(Math.Ceiling(((BreakPercent * ProductTypeKef) + BreakPercent) * NumberOfProducts));
+
+            material.counter = MaterialsForOneProduct;
+            materials.Add(material);
+        }
+
+        private void CountBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var db = Helper.GetContext();
+            var material = db.ProductsMaterial.Where(x => x.ProductID == product_.ID).ToList();
+            foreach (var item in material)
+            {
+                MaterialsCount(product_.ProductTypeID, item.MaterialID, Convert.ToInt32(CountBox.Text), item.Materials.Remains, item.Materials.MaterialsType.BreakPercent, product_.ProductType.ProductTypeCoef, db);
+            }
+            LVMaterials.ItemsSource = materials;
         }
     }
 }
